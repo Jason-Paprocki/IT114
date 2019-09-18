8187fd4f19f5ecc850e0cf0a6e0f6ceb5f83c06d
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-09-18 09:35:35 -0400

    Work on HW2

--- Homework2.java
+++ Homework2.java
@@ -1,6 +1,13 @@
 public class Homework2
 {
   public static void main(String[] args) {
-    
+    //create queue
+     Queue<String> StringQueue = new LinkedList<>();
+
+     StringQueue.add("Element 1");
+     StringQueue.add("Element 2");
+     StringQueue.add("Element 3");
+
+     
   }
 }
