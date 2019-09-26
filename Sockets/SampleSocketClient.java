3027e67bda375ce9ad375f84f06c9e028a5f164c
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-09-26 14:13:55 -0400

    added progect proposal

--- Sockets/SampleSocketClient.java
+++ Sockets/SampleSocketClient.java
@@ -9,8 +9,9 @@ import java.util.Scanner;
 public class SampleSocketClient {
 	Socket server;
 	public SampleSocketClient() {
-
 	}
+
+
 	public void connect(String address, int port) {
 		try {
 			server = new Socket(address, port);
@@ -21,33 +22,78 @@ public class SampleSocketClient {
 			e.printStackTrace();
 		}
 	}
+
+
 	public void start() throws IOException {
 		if(server == null) {
 			return;
 		}
-		System.out.println("Listening for input");
+		System.out.println("Client Started");
 		try(Scanner si = new Scanner(System.in);
 				PrintWriter out = new PrintWriter(server.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));){
-			String line = "";
-			while(true) {
-				try {
-					System.out.println("Waiting for input");
-					line = si.nextLine();
-					if(!"quit".equalsIgnoreCase(line)) {
-						out.println(line);
+			//Thread to listen for keyboard input so main thread isn't blocked
+			Thread inputThread = new Thread() {
+				@Override
+				public void run() {
+					try {
+						while(!server.isClosed()) {
+							System.out.println("Waiting for input");
+							String line = si.nextLine();
+							if(!"quit".equalsIgnoreCase(line) && line != null) {
+								out.println(line);
+							}
+							else {
+								System.out.println("Stopping input thread");
+								break;
+							}
+						}
 					}
-					else {
-						break;
+					catch(Exception e) {
+						System.out.println("Client shutdown");
 					}
-					line = "";
+					finally {
+						close();
+					}
 				}
-				catch(Exception e) {
-					System.out.println("Connection dropped");
-					break;
+			};
+			inputThread.start();//start the thread
+
+			//Thread to listen for responses from server so it doesn't block main thread
+			Thread fromServerThread = new Thread() {
+				@Override
+				public void run() {
+					try {
+						String fromServer = "";
+						while(!server.isClosed() && (fromServer = in.readLine()) != null) {
+							System.out.println("Replay from server: " + fromServer);
+						}
+						System.out.println("Stopping server listen thread");
+					}
+					catch (Exception e) {
+						if(!server.isClosed()) {
+							e.printStackTrace();
+							System.out.println("Server closed connection");
+						}
+						else {
+							System.out.println("Connection closed");
+						}
+					}
+					finally {
+						close();
+					}
 				}
+			};
+			fromServerThread.start();//start the thread
+
+			//Keep main thread alive until the socket is closed
+			while(!server.isClosed()) {
+				Thread.sleep(50);
 			}
 			System.out.println("Exited loop");
+			System.exit(0);//force close
+			//TODO implement cleaner closure when server stops before client
+			//currently hangs/waits on the console/scanner input
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -57,7 +103,7 @@ public class SampleSocketClient {
 		}
 	}
 	private void close() {
-		if(server != null) {
+		if(server != null && !server.isClosed()) {
 			try {
 				server.close();
 				System.out.println("Closed socket");
@@ -67,14 +113,36 @@ public class SampleSocketClient {
 		}
 	}
 	public static void main(String[] args) {
-		SampleSocketClient client = new SampleSocketClient();
-		client.connect("127.0.0.1", 3002);
-		try {
-			//if start is private, it's valid here since this main is part of the class
-			client.start();
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		for(int x = 0; x <= 20; x++)
+		{
+			SampleSocketClient client = new SampleSocketClient();
+			int port = -1;
+			try{
+				//not safe but try-catch will get it
+				port = Integer.parseInt(args[0]);
+			}
+			catch(Exception e){
+				System.out.println("Invalid port");
+			}
+			if(port == -1){
+				return;
+			}
+			client.connect("128.235.211.21", port);
+			try {
+				//if start is private, it's valid here since this main is part of the class
+				client.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+
+
+
+
+
+
+
 	}
 
 }
