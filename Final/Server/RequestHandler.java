7aa34c76be712e2d5cf60cbe59cb8d49916de6cf
desktop <desktop@pop-os.localdomain>
2019-11-20 17:28:47 -0500

    beep

--- Final/Server/RequestHandler.java
+++ Final/Server/RequestHandler.java
@@ -386,6 +386,7 @@ public class RequestHandler implements Runnable
 				int read;
 				do {
 					read = proxyToClientIS.read(buffer);
+					System.out.println("Beep");
 					if (read > 0) {
 						proxyToServerOS.write(buffer, 0, read);
 						if (proxyToClientIS.available() < 1) {
