e85821eca79ebcbdab4099cb6d77426c08b9261e
laptop <laptop@pop-os.localdomain>
2019-11-26 13:45:20 -0500

    almost work

--- Project Proxy/UI.java
+++ Project Proxy/UI.java
@@ -5,47 +5,55 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UI
 {
 
 	public static void main(String[] args)
 	{
+		//crete the Java frame and panel
 		JFrame frame = new JFrame("A Simple GUI");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 500);
+		frame.setSize(1500, 500);
 		frame.setLocation(430, 100);
-
 		JPanel panel = new JPanel();
-
 		frame.add(panel);
 
+		//label to specify server
 		JLabel lbl = new JLabel("Select one server ");
 		lbl.setVisible(true);
-
 		panel.add(lbl);
 
-		String[] choices = { "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
+		//add servers here
+		HashMap<String, String> addresses  = new HashMap<>();
+		addresses.put("Server 1", "127.0.0.1");
 
+		//show choices with a combo box
+		String[] choices = { "Server 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
 		JComboBox<String> cb = new JComboBox<String>(choices);
-
 		cb.setVisible(true);
 		panel.add(cb);
 
+		//show button for selecting
 		JButton btn = new JButton();
-		btn.setText("CONECT");
+		btn.setText("CONNECT");
 		panel.add(btn);
-
+		//onclick action
 		btn.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				server = choice;
+				String whichServer = cb.getSelectedItem().toString();
+				String ipAddress = addresses.get(whichServer);
+				Client(ipAddress, 8080);
 			}
 
 		});
 
+		frame.pack();
 	}
 }
